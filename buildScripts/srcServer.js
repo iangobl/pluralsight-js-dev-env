afd5562bef14bd4cd2e1a185914fc1f86e43a63c
Ian Gobl <iangobl@hotmail.com>
2018-04-06 14:30:18 +0100

    Populate users from api call

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res){
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName":"Joe", "lastName":"Morris", "email":"joe@fake.com"},
+    {"id": 2, "firstName":"Richard", "lastName":"Morris", "email":"rich@fake.com"},
+    {"id": 3, "firstName":"Ciara", "lastName":"Morris", "email":"ciara@fake.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(chalk.green(err));
