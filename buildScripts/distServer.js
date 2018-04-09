caa057d8facdb343a219edb2a78dc7eba9cfa737
Ian Gobl <iangobl@hotmail.com>
2018-04-09 10:12:58 +0100

    1. Use HtmlWebpackPlugin to automatically refernce the bundles javascript instead of hardcoding reference to bundle.js 2. Setup scripts to build application 3. Create seperate budles for vendor libraries (e.g. jquery) so that large vendor files do not need to be redownloaded multiple times.

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -10,7 +10,7 @@ const port = 3000;
 const app = express();
 
 app.use(compression());
-app.use(express.statis('dist'));
+app.use(express.static('dist'));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../dist/index.html'));
